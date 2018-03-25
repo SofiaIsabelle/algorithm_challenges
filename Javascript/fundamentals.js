6353226cedc058f3227a6531a66bc0174dbef6a3
Nina Rahimi <nina.i.rahimi@gmail.com>
2018-03-24 21:31:44 -0400

    Biggie Size

--- Javascript/fundamentals.js
+++ Javascript/fundamentals.js
@@ -4,7 +4,7 @@ makseItBig([-1,3,5,-5]) returns that same array changed to ([-1, "big", "big", -
 
 function biggieSize(array){
     length = array.length;
-    for(var i = 0; i < length; i++){
+    for(var i = 0; i < array.length; i++){
         if(array[i] > 0){
             array[i] = "big";
         }
@@ -13,3 +13,5 @@ function biggieSize(array){
 }
 
 console.log(biggieSize([-3, -1, 5, 6, 7, 8, -2, -5]));
+
+
